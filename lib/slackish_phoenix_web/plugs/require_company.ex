6274a2794077ac07b44fda9675db216773cf83ea
Tony Messias <tonyzrp@gmail.com>
2018-12-11 21:36:18 -0300

    Small refactoring

--- lib/slackish_phoenix_web/plugs/require_company.ex
+++ lib/slackish_phoenix_web/plugs/require_company.ex
@@ -10,25 +10,22 @@ defmodule SlackishPhoenixWeb.Plugs.RequireCompany do
   def call(conn, _params) do
     case conn.assigns[:user] do
       nil
-        -> conn |> redirect_and_halt(Helpers.home_path(conn, :index))
-      user
         -> conn
-          |> validate_has_company(user)
+           |> redirect(to: Helpers.home_path(conn, :index))
+           |> halt
+      user
+        -> conn |> validate_has_company(user)
     end
   end
 
   defp validate_has_company(conn, user) do
     case user.current_company_id do
       nil
-        -> conn |> redirect_and_halt(Helpers.company_path(conn, :new))
+        -> conn
+           |> redirect(to: Helpers.company_path(conn, :new))
+           |> halt
       _id
         -> conn
     end
-  end
-
-  defp redirect_and_halt(conn, path) do
-    conn
-      |> redirect(to: path)
-      |> halt()
   end
 end
