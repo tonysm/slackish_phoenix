e67bedb9289348a70d17d3aba2894e561fc90a9b
Tony Messias <tonyzrp@gmail.com>
2018-12-18 00:43:49 -0300

    Test sending messages to channels

--- lib/slackish_phoenix_web/channels/channel_channel.ex
+++ lib/slackish_phoenix_web/channels/channel_channel.ex
@@ -3,8 +3,8 @@ defmodule SlackishPhoenixWeb.ChannelChannel do
 
   alias SlackishPhoenix.Auth
 
-  def join("channels:" <> channel_id, payload, socket) do
-    {:ok, %{}, assign(socket, :channel_id, channel_id)}
+  def join("channels:" <> channel_id, _payload, socket) do
+    {:ok, %{}, assign(socket, :channel_id, channel_id |> String.to_integer())}
   end
 
   def handle_in(_name, %{"message" => message, "uuid" => id}, socket) do
@@ -12,14 +12,12 @@ defmodule SlackishPhoenixWeb.ChannelChannel do
 
     user = Auth.get_user!(socket.assigns[:user_id])
 
-    payload = %{
+    broadcast!(socket, "channels:#{channel_id}:new_message", %{
       id: id,
       message: message,
       user: user,
       sent_at: inspect(System.system_time(:second))
-    }
-
-    broadcast!(socket, "channels:#{channel_id}:new_message", payload)
+    })
 
     {:reply, :ok, socket}
   end
--- test/slackish_phoenix_web/channels/channel_channel_test.exs
+++ test/slackish_phoenix_web/channels/channel_channel_test.exs
@@ -2,25 +2,63 @@ defmodule SlackishPhoenixWeb.ChannelChannelTest do
   use SlackishPhoenixWeb.ChannelCase
 
   setup do
+    user = user_fixture()
+    company = company_fixture(%{owner_id: user.id})
+    channel = channel_fixture(%{company_id: company.id})
+
     {:ok, _, socket} =
-      socket(SlackishPhoenixWeb.UserSocket, "user_id", %{some: :assign})
-      |> subscribe_and_join(SlackishPhoenixWeb.ChannelChannel, "channel:lobby")
+      socket(SlackishPhoenixWeb.UserSocket, "usertoken", %{user_id: user.id})
+      |> subscribe_and_join(SlackishPhoenixWeb.ChannelChannel, "channels:#{channel.id}")
 
-    {:ok, socket: socket}
+    {:ok, socket: socket, company: company, user: user, channel: channel}
   end
 
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push(socket, "ping", %{"hello" => "there"})
-    assert_reply ref, :ok, %{"hello" => "there"}
+  defp user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        email: "some email",
+        google_id: "some google_id",
+        image_url: "some image_url",
+        name: "some name"
+      })
+      |> SlackishPhoenix.Auth.create_user()
+
+    user
   end
 
-  test "shout broadcasts to channel:lobby", %{socket: socket} do
-    push(socket, "shout", %{"hello" => "all"})
-    assert_broadcast "shout", %{"hello" => "all"}
+  defp company_fixture(attrs) do
+    {:ok, company} =
+      attrs
+      |> Enum.into(%{name: "madewithlove"})
+      |> SlackishPhoenix.Companies.create_company()
+
+    company
   end
 
-  test "broadcasts are pushed to the client", %{socket: socket} do
-    broadcast_from!(socket, "broadcast", %{"some" => "data"})
-    assert_push "broadcast", %{"some" => "data"}
+  defp channel_fixture(attrs) do
+    {:ok, channel} =
+      attrs
+      |> Enum.into(%{name: "general"})
+      |> SlackishPhoenix.Chat.create_channel()
+
+    channel
+  end
+
+  test "join/3 assigns the channel_id to the socket", %{socket: socket, channel: channel} do
+    assert socket.assigns[:channel_id] == channel.id
+  end
+
+  test "handle_in/3 broadcasts the given message", %{socket: socket, channel: channel, user: user} do
+    push(socket, "channels:#{channel.id}", %{
+      "message" => "hello from the other side",
+      "uuid" => "some-fake-uuid"
+    })
+
+    assert_broadcast _, event
+
+    assert event.id == "some-fake-uuid"
+    assert event.message == "hello from the other side"
+    assert event.user.id == user.id
   end
 end
