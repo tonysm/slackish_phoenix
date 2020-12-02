b3c160e3cc124fba83837db0d0fa5b6f652ae236
Tony Messias <tonysm@hey.com>
2020-12-02 10:13:15 -0300

    Add google scope and fix tailwind version

--- config/config.exs
+++ config/config.exs
@@ -27,7 +27,7 @@ config :phoenix, :json_library, Jason
 
 config :ueberauth, Ueberauth,
   providers: [
-    google: {Ueberauth.Strategy.Google, []}
+    google: {Ueberauth.Strategy.Google, [default_scope: "email profile"]}
   ]
 
 # Import environment specific config. This must remain at the bottom
